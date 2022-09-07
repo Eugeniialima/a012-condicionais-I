a46b72ce7a08ab7e6e1c46b192b9f330e06ac46c
MariaEugenia <eugeniialima@gmail.com>
2022-09-06 22:02:07 -0300

    Finalmente acabei

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qual é sua idade?")
+let ensinoMedio = confirm("Você terminou o ensino médio?")
+let cursandoFaculdade = confirm("Você está cursando alguma faculdade?")
 
-if(){
 
-}else{
-	
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Barrado no baile")
+}
+
+if (ensinoMedio) {
+    console.log("Parabéns")
+} else {
+    console.log("Tente outra vez")
+}
+
+if (!cursandoFaculdade) {
+    console.log("Vai estudar!")
+} else {
+    console.log("Muito bem")
 }
